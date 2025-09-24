493f761f9382a4070a93dc366e7da48b04ad7e09
ALEXISBRUNO-code <velardealeno18@autonoma.edu.pe>
2025-09-23 20:46:02 -0500

    CARGA INICIAL

--- src/Programas/Operaciones.java
+++ src/Programas/Operaciones.java
@@ -4,18 +4,20 @@ import java.util.Scanner;
 
 public class Operaciones {
     public static void main(String[] args) {
-        int n1, n2, suma;
+        int n1, n2, suma, resta;
 
         Scanner lectura = new Scanner(System.in);
 
-        System.out.print("INGRESA EL PRIMER NUMERO: ");
+        System.out.print("INGRESA EL PRIMER NUMERO 1: ");
         n1 = lectura.nextInt();
 
-        System.out.print("INGRESA EL SEGUNDO NUMERO: ");
+        System.out.print("INGRESA EL SEGUNDO NUMERO 2: ");
         n2 = lectura.nextInt();
 
         suma = n1 + n2;
+        resta = n1 - n2;
 
         System.out.println("LA SUMA ES: " + suma);
+        System.out.println("LA RESTA ES: " + resta);
     }
 }
